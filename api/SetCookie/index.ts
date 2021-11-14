e183b33d5a485c51736d030bdfaaa9beb3314f6b
Gilbert <gilbertappiah@icloud.com>
2021-11-13 19:40:35 -0500

    Update index.ts

--- api/SetCookie/index.ts
+++ api/SetCookie/index.ts
@@ -2,23 +2,17 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   context.log('HTTP trigger function processed a request.')
-
+  
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: 'Hello, world.',
+    body: 'Hello, world.' + req.cookies,
     cookies: [
-      {
-        name: 'HOST',
-        value: 'value1',
-        secure: true,
-        domain: 'blue-ocean-01ce8ea10.azurestaticapps.net',
-        path:'/'
-      },
        {
         name: '__HOST-Testing',
         value: 'value2',
         secure: true,
-        //domain: 'azurestaticapps.net' // lol wont work
+        domain: '',
+        path: '/'// lol wont work
       },{
         name: '__Secure-Testing',
         value: 'value2',
